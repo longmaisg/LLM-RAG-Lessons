7974a5a51316fd52d07fdc3c9c70f3c91df68f1f
long.mai <long.mai@realtimeatwork.com>
2025-04-04 14:58:58 +0200

    update chatgpt class

--- chatgpt.py
+++ chatgpt.py
@@ -2,8 +2,17 @@ import openai
 import os
 from conversation import Conversation
 
-# Set up OpenAI API key
-openai.api_key = os.getenv("OPENAI_API_KEY")  # Or use environment variable method
+class Conversation:
+    messages = []
+    system_prompt = "You are a helpful assistant. You answer will be short and no more than 20 words."
+
+    def __init__(self):
+        self.messages = [{"role": "system", "content": self.system_prompt}]
+
+    def add_message(self, message, role="user"):
+        if role not in ["user", "assistant"]:
+            raise ValueError("Role must be either 'user' or 'assistant'")
+        self.messages.append({"role": role, "content": message})
 
 def get_chat_completion(messages=list, model="gpt-4o-mini") -> str:
     try:
@@ -17,19 +26,33 @@ def get_chat_completion(messages=list, model="gpt-4o-mini") -> str:
         return response.choices[0].message.content
     except Exception as e:
         return str(e)
+    
+def setup_chatgpt():
+    # Set up OpenAI API key
+    openai.api_key = os.getenv("OPENAI_API_KEY")  # Or use environment variable method
+
+    # start a new conversation
+    conversation = Conversation()
+    return conversation
 
 # Example usage
-conversation = Conversation()
-user_prompt = "one plus one is how much?"
-print("\nuser: ", user_prompt)
-conversation.add_message(user_prompt, "user")
-response = get_chat_completion(conversation.messages)
-print("\nassistant: ", response)
-conversation.add_message(response, "assistant")
+if __name__ == "__main__":
 
-user_prompt = "my wife says one plus one is three, is she right?"
-print("\nuser: ", user_prompt)
-conversation.add_message(user_prompt, "user")
-response = get_chat_completion(conversation.messages)
-print("\nassistant: ", response)
-conversation.add_message(response, "assistant")
+    # Set up OpenAI API key
+    openai.api_key = os.getenv("OPENAI_API_KEY")  # Or use environment variable method
+
+    # Example conversation
+    conversation = Conversation()
+    user_prompt = "one plus one is how much?"
+    print("\nuser: ", user_prompt)
+    conversation.add_message(user_prompt, "user")
+    response = get_chat_completion(conversation.messages)
+    print("\nassistant: ", response)
+    conversation.add_message(response, "assistant")
+
+    user_prompt = "my wife says one plus one is three, is she right?"
+    print("\nuser: ", user_prompt)
+    conversation.add_message(user_prompt, "user")
+    response = get_chat_completion(conversation.messages)
+    print("\nassistant: ", response)
+    conversation.add_message(response, "assistant")
